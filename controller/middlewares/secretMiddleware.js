5cc99f4287df57cbe7a0326cb40b8b2a444633c8
webdev-hemant <webdev.hemant@gmail.com>
2022-12-03 21:59:44 +0530

    project fixed

--- controller/middlewares/secretMiddleware.js
+++ controller/middlewares/secretMiddleware.js
@@ -1,7 +1,11 @@
 const userQuieries = require("../../model/users.quiries");
 
 const postMiddleware = (req, res, next) => {
-  const data = { name: req.body.name, age: req.body.age };
+  const data = {
+    id: req.body.id,
+    name: req.body.name,
+    address: req.body.address,
+  };
 
   userQuieries.addDataSchema(data, (isSucceed) => {
     req.isSucceed = isSucceed;
